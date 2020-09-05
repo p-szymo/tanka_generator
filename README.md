15dec837e27c47da3b0db78f6ac2c720e1d20287
“p-szymo” <joshua.szymanowski@gmail.com>
2020-09-05 19:32:48 -0400

    update gitignore, create readme, upload data, add old code to experiments, update functions

---
+++ README.md
@@ -0,0 +1,7 @@
+# tankanizer
+
+Using Allison Parrish's [Pronouncing](https://github.com/aparrish/pronouncingpy) to count syllables, I experiment with generating poetry in the tanka form using several corpora and a Markov chain.
+
+Thinking as I'm typing that perhaps I can use the Markov chain to generate poetry that I can then use to train a more sophisticated RNN generator. Some sort of text generation loop.
+
+Early stages!
--- functions.py
+++ functions.py
@@ -5,111 +5,141 @@ import re
 import random
 from pronouncing import phones_for_word, syllable_count
 
-## call API and create a list of authors
-def author_grabber(url):
-#     url = "http://poetrydb.org/author"
-    authors = requests.get(url)
-    authors = [author for author in authors.json()['authors']]
-    return authors
+
+def value_tester(values):
+    valid = False
+    for word in values:
+        if phones_for_word(word):
+            valid = True
+            break
+        else:
+            continue
+    return valid
 
 
-## call API and create a list of poem titles for each author
-def title_grabber(author):
-    base_url = "http://poetrydb.org/author/"
-    url_addon = (author + '/title').replace(" ", "%20")
-    try:
-        titles = requests.get(base_url + url_addon)
-        titles = [title['title'] for title in titles.json()]
-    except:
-        print(author)
-        pass
-    return titles
+def line_creator(word_dict, num_syllables, prompt=None):
+    '''Input a dictionary in the format of key = current word, value = list of next words
+       along with the number of words you would like to see in your generated sentence.'''
+    
+    line = []
+    count = 0
+    
+    if prompt:
+        while count == 0:
+            try:
+                start_word = random.choice(word_dict[prompt])
+                count = syllable_count(phones_for_word(start_word)[0])
+                if count <= num_syllables:
+                    line.append(start_word)
+                else:
+                    count = 0
+                    continue
+            except IndexError:
+                continue
+              
+    else:
+        while count == 0:
+            try:
+                start_word = random.choice(list(word_dict.keys()))
+                count = syllable_count(phones_for_word(start_word)[0])
+                if count <= num_syllables:
+                    line.append(start_word)
+                else:
+                    count = 0
+                    continue
+            except IndexError:
+                print('initial word index error')
+                count = 0
+                continue
+
+    while count < num_syllables:
+#         try:
+        next_word = random.choice(word_dict[line[-1]])
+        if not value_tester(word_dict[next_word]):
+            next_word = 'the'
+        temp_count = count + syllable_count(phones_for_word(next_word)[0])
+        if temp_count <= num_syllables:
+            line.append(next_word)
+            count += syllable_count(phones_for_word(next_word)[0])
+        else:
+            continue
+#         except IndexError:
+#             print('next word index error')
+#             count = count
+#             continue
+            
+    return line
 
 
-## call API and create a list of the lines of a poem
-## convert list to a string, joining them with '\n' 
-def poem_grabber(title):
-    base_url = 'http://poetrydb.org/title/'
-    url_addon = (title + '/lines.json').replace(" ", "%20").replace(')', '').replace(']', '').split('/', 1)[0].split('^', 1)[0]
-    try:
-        response = requests.get(base_url + url_addon)
-        lines = response.json()[0]['lines']
-        poem = " \n ".join(lines).lower()
-        poem = poem.translate(str.maketrans('', '', string.punctuation)).replace(
-            '  ', ' \t ').replace('—', '').replace('‘','').replace('’','')
-    except:
-        print(title)
-        pass
-    return poem
+def line_creator_DEFUNCT(word_dict, num_syllables, prompt=None):
+    '''Input a dictionary in the format of key = current word, value = list of next words
+       along with the number of words you would like to see in your generated sentence.'''
+    
+    line = []
+    count = 0
+    
+    if prompt:
+        if not value_tester(word_dict[prompt]):
+            prompt = 'the'
+            
+        while count == 0:
+            try:
+                start_word = random.choice(word_dict[prompt])
+                count = syllable_count(phones_for_word(start_word)[0])
+                if count <= num_syllables:
+                    line.append(start_word)
+                else:
+                    count = 0
+                    continue
+            except IndexError:
+                continue
+              
+    else:
+        print('into else') 
+        while count == 0:
+            try:
+                start_word = random.choice(list(word_dict.keys()))
+#                 if value_tester(word_dict[start_word]):
+                    
+                count = syllable_count(phones_for_word(start_word)[0])
+                if count <= num_syllables:
+                    line.append(start_word)
+                else:
+                    count = 0
+                    continue
+            except IndexError:
+                print('initial word index error')
+                count = 0
+                continue
 
-
-## temporarily change newline and tab characters so they don't disappear during segmentation
-def lines_tabs_creator(po_dict):
-    new_line = 'airplane'
-    tab = 'automobile'
-    for word, followers in po_dict.items():
-        for i,follower in enumerate(followers):
-            if follower == new_line:
-                followers[i] = '\n'
-            elif follower == tab:
-                followers[i] = '\t'
+    while count < num_syllables:
+        try:
+            next_word = random.choice(word_dict[line[-1]])
+            if not value_tester(word_dict[next_word]):
+                next_word = 'the'
+            temp_count = count + syllable_count(phones_for_word(next_word)[0])
+            if temp_count <= num_syllables:
+                line.append(next_word)
+                count += syllable_count(phones_for_word(next_word)[0])
             else:
                 continue
-    return po_dict
-
-
-## ask the user to provide a length for the poem (in numerals)
-def word_quantifier():
-    while True:
-        try:
-            word_count = int(input("What length doth thy sweet nothings require? "))
-            while word_count > 1000:
-                word_count = int(input("Surely thine sweet nothings require less breadth!\nCease skylarking and present me a length of reason! "))
-        except ValueError:
-            print("Art thou a dullard? Enumerate!")
+        except IndexError:
+            print('next word index error')
+            count = count
             continue
-        else:
-            break
-    return word_count
+            
+    return line
 
 
-## generate poetry from dictionary of words that appear in the PoetryDB's archive of poems
-def tankanizer(po_dict):
+def tankanizer(word_dict, start_word=None):
 
-    tanka = []
-    line_1 = []
-    line_2 = []
-    line_3 = []
-    line_4 = []
-    line_5 = []
+    line_1 = line_creator(word_dict, 5, start_word)
+    line_2 = line_creator(word_dict, 7, line_1[-1])
+    line_3 = line_creator(word_dict, 5, line_2[-1])
+    line_4 = line_creator(word_dict, 7, line_3[-1])
+    line_5 = line_creator(word_dict, 7, line_4[-1])
+    lines = [line_1, line_2, line_3, line_4, line_5]
+    tanka_lines = [(' ').join(line) for line in lines]
+    tanka = '\n'.join(tanka_lines)
     
-    syllables = 0
-    while syllables == 0:
-        first_word = random.choice(list(po_dict.keys()))
-        syl_count = syllable_count(phones_for_word(first_word)[0])
-        if syl_count <= 5:
-            line_1.append(first_word)
-            syllables += fw_syl_count
-        else:
-            continue
-    
-    while syllables < 5:
-        next_word = random.choice(po_dict[line_1[-1]])
-        syl_count = syllable_count(phones_for_word(next_word)[0])
-        if syl_count <= (5 - syllables):
-            line_1.append(next_word)
-            syllables += fw_syl_count
-        else:
-            continue
-    
-    print((' ').join(line_1))
-#         while syl_count
-#         syllable_count < (5 - fw_syl_count)
-
-#     while len(auto_pome) < word_count:
-#         next_word = random.choice(po_dict[auto_pome[-1]])
-#         auto_pome.append(next_word)
-#     print('\n\n\n ' + (' ').join(auto_pome))
-    
-# pronunciation_list = pronouncing.phones_for_word("programming")
-# pronouncing.syllable_count(pronunciation_list[0])
+    return tanka
