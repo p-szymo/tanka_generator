8b6a1dbdaf1081bf77b5c7c73d00348e726be4c8
“p-szymo” <joshua.szymanowski@gmail.com>
2020-09-07 16:23:31 -0400

    overhaul/simplify functions, create clean notebook with generations

--- functions.py
+++ functions.py
@@ -1,21 +1,50 @@
-import requests
-import json
-import string
-import re
 import random
+from collections import defaultdict
 from pronouncing import phones_for_word, syllable_count
 
 
-def value_tester(values):
-    valid = False
-    for word in values:
-        if phones_for_word(word):
-            valid = True
-            break
-        else:
-            continue
-    return valid
 
+def countable_corpus(corpus):
+    # instantiate a dictionary
+    text_dictionary = defaultdict(list)
+
+    # create Markov dictionary
+    # iterate over list of each word and its subsequent word
+    for current_word, next_word in zip(corpus, corpus[1:]):
+
+        # append word to list as long as its phonemes can be counted
+        if phones_for_word(next_word):
+            text_dictionary[current_word].append(next_word)
+
+        # otherwise choose a random word from the corpus whose phonemes can
+        # NOTE: introduces some randomness! preventing assured reproducibility
+        else:
+            word = ''
+            while not word:
+                word = random.choice(corpus)
+                if phones_for_word(word):
+                    text_dictionary[current_word].append(word)
+                else:
+                    word = ''
+                
+    return text_dictionary
+
+
+def word_grabber(word_list, num_syllables):
+    
+    iters = 0
+    n = len(word_list)
+    word = random.choice(word_list)
+    count = syllable_count(phones_for_word(word)[0])
+    while iters < 2*n:
+        if count <= num_syllables:
+            return word, count
+        else:
+            iters += 1
+            continue
+    
+    return ('the', 1)
+            
 
 def line_creator(word_dict, num_syllables, prompt=None):
     '''Input a dictionary in the format of key = current word, value = list of next words
@@ -25,108 +54,19 @@ def line_creator(word_dict, num_syllables, prompt=None):
     count = 0
     
     if prompt:
-        while count == 0:
-            try:
-                start_word = random.choice(word_dict[prompt])
-                count = syllable_count(phones_for_word(start_word)[0])
-                if count <= num_syllables:
-                    line.append(start_word)
-                else:
-                    count = 0
-                    continue
-            except IndexError:
-                continue
-              
+        word_list = word_dict[prompt]
+        
     else:
-        while count == 0:
-            try:
-                start_word = random.choice(list(word_dict.keys()))
-                count = syllable_count(phones_for_word(start_word)[0])
-                if count <= num_syllables:
-                    line.append(start_word)
-                else:
-                    count = 0
-                    continue
-            except IndexError:
-                print('initial word index error')
-                count = 0
-                continue
+        word_list = [word for word in list(word_dict.keys()) if phones_for_word(word)]
+              
+    word, count = word_grabber(word_list, num_syllables)
+    line.append(word)
 
     while count < num_syllables:
-#         try:
-        next_word = random.choice(word_dict[line[-1]])
-        if not value_tester(word_dict[next_word]):
-            next_word = 'the'
-        temp_count = count + syllable_count(phones_for_word(next_word)[0])
-        if temp_count <= num_syllables:
-            line.append(next_word)
-            count += syllable_count(phones_for_word(next_word)[0])
-        else:
-            continue
-#         except IndexError:
-#             print('next word index error')
-#             count = count
-#             continue
-            
-    return line
-
-
-def line_creator_DEFUNCT(word_dict, num_syllables, prompt=None):
-    '''Input a dictionary in the format of key = current word, value = list of next words
-       along with the number of words you would like to see in your generated sentence.'''
-    
-    line = []
-    count = 0
-    
-    if prompt:
-        if not value_tester(word_dict[prompt]):
-            prompt = 'the'
-            
-        while count == 0:
-            try:
-                start_word = random.choice(word_dict[prompt])
-                count = syllable_count(phones_for_word(start_word)[0])
-                if count <= num_syllables:
-                    line.append(start_word)
-                else:
-                    count = 0
-                    continue
-            except IndexError:
-                continue
-              
-    else:
-        print('into else') 
-        while count == 0:
-            try:
-                start_word = random.choice(list(word_dict.keys()))
-#                 if value_tester(word_dict[start_word]):
-                    
-                count = syllable_count(phones_for_word(start_word)[0])
-                if count <= num_syllables:
-                    line.append(start_word)
-                else:
-                    count = 0
-                    continue
-            except IndexError:
-                print('initial word index error')
-                count = 0
-                continue
-
-    while count < num_syllables:
-        try:
-            next_word = random.choice(word_dict[line[-1]])
-            if not value_tester(word_dict[next_word]):
-                next_word = 'the'
-            temp_count = count + syllable_count(phones_for_word(next_word)[0])
-            if temp_count <= num_syllables:
-                line.append(next_word)
-                count += syllable_count(phones_for_word(next_word)[0])
-            else:
-                continue
-        except IndexError:
-            print('next word index error')
-            count = count
-            continue
+        
+        next_word, update = word_grabber(word_dict[line[-1]], num_syllables-count)
+        line.append(next_word)
+        count += update 
             
     return line
 
@@ -143,3 +83,141 @@ def tankanizer(word_dict, start_word=None):
     tanka = '\n'.join(tanka_lines)
     
     return tanka
+
+
+
+# def value_tester(values):
+#     valid = False
+#     for word in values:
+#         if phones_for_word(word):
+#             valid = True
+#             break
+#         else:
+#             continue
+#     return valid
+
+
+# def line_creator_ORIGINAL(word_dict, num_syllables, prompt=None):
+#     '''Input a dictionary in the format of key = current word, value = list of next words
+#        along with the number of words you would like to see in your generated sentence.'''
+    
+#     line = []
+#     count = 0
+    
+#     if prompt:
+#         while count == 0:
+#             try:
+#                 start_word = random.choice(word_dict[prompt])
+#                 count = syllable_count(phones_for_word(start_word)[0])
+#                 if count <= num_syllables:
+#                     line.append(start_word)
+#                 else:
+#                     count = 0
+#                     continue
+#             except IndexError:
+#                 continue
+              
+#     else:
+#         while count == 0:
+#             try:
+#                 start_word = random.choice(list(word_dict.keys()))
+#                 count = syllable_count(phones_for_word(start_word)[0])
+#                 if count <= num_syllables:
+#                     line.append(start_word)
+#                 else:
+#                     count = 0
+#                     continue
+#             except IndexError:
+#                 count = 0
+#                 continue
+
+#     while count < num_syllables:
+        
+#         next_word = random.choice(word_dict[line[-1]])
+#         print(next_word)
+# #         if not value_tester(word_dict[next_word]):
+# #             next_word = 'the'
+#         word_syll_count = syllable_count(phones_for_word(next_word)[0])
+#         count += word_syll_count
+#         if count <= num_syllables:
+#             line.append(next_word)
+#         else:
+#             count -= word_syll_count
+#             continue
+# #         try:
+# #         next_word = random.choice(word_dict[line[-1]])
+# #         if not phones_for_word(next_word):
+# #             next_word = 'the'
+# #         print(next_word)
+# #         temp_count = count + syllable_count(phones_for_word(next_word)[0])
+# #         if temp_count <= num_syllables:
+# #             line.append(next_word)
+# #             count += syllable_count(phones_for_word(next_word)[0])
+# #         else:
+# #             continue
+# #         except IndexError:
+# #             print('next word index error')
+# #             count = count
+# #             continue
+            
+#     return line
+
+
+# def line_creator_DEFUNCT(word_dict, num_syllables, prompt=None):
+#     '''Input a dictionary in the format of key = current word, value = list of next words
+#        along with the number of words you would like to see in your generated sentence.'''
+    
+#     line = []
+#     count = 0
+    
+#     if prompt:
+#         if not value_tester(word_dict[prompt]):
+#             prompt = 'the'
+            
+#         while count == 0:
+#             try:
+#                 start_word = random.choice(word_dict[prompt])
+#                 count = syllable_count(phones_for_word(start_word)[0])
+#                 if count <= num_syllables:
+#                     line.append(start_word)
+#                 else:
+#                     count = 0
+#                     continue
+#             except IndexError:
+#                 continue
+              
+#     else:
+#         print('into else') 
+#         while count == 0:
+#             try:
+#                 start_word = random.choice(list(word_dict.keys()))
+# #                 if value_tester(word_dict[start_word]):
+                    
+#                 count = syllable_count(phones_for_word(start_word)[0])
+#                 if count <= num_syllables:
+#                     line.append(start_word)
+#                 else:
+#                     count = 0
+#                     continue
+#             except IndexError:
+#                 print('initial word index error')
+#                 count = 0
+#                 continue
+
+#     while count < num_syllables:
+#         try:
+#             next_word = random.choice(word_dict[line[-1]])
+#             if not value_tester(word_dict[next_word]):
+#                 next_word = 'the'
+#             count += syllable_count(phones_for_word(next_word)[0])
+#             if count <= num_syllables:
+#                 line.append(next_word)
+#             else:
+#                 count -= syllable_count(phones_for_word(next_word)[0])
+#                 continue
+#         except IndexError:
+#             print('next word index error')
+#             count = count
+#             continue
+            
+#     return line
